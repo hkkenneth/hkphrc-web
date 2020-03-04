3a2e2ef4e47bc840f380da4e87069023fd3bbddf
OpenHealth HK <openhealthhk@gmail.com>
2020-03-05 02:02:53 +0800

    Commit build with English version

--- styles-2a6e1feaf7d5d600a350.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{gcSU:function(n,o,c){},qZkI:function(n,o,c){},"sR/7":function(n,o,c){}}]);
-//# sourceMappingURL=styles-2a6e1feaf7d5d600a350.js.map
---
+++ styles-69d6ff2bf9ae727859c0.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{gcSU:function(n,o,c){},qZkI:function(n,o,c){},"sR/7":function(n,o,c){}}]);
+//# sourceMappingURL=styles-69d6ff2bf9ae727859c0.js.map
